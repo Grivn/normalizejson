39ce3850a6059e03c31be8b27a142d1f97974299
Grivn Wang <grivn.wang@gmail.com>
2023-03-07 16:16:45 +0800

    test(format_data): unit test for update template for provider

--- format_data_test.go
+++ format_data_test.go
@@ -70,6 +70,65 @@ func TestFormatDataProvider(t *testing.T) {
 	assert.Equal(t, formatJSON(result), formatJSON(formatted))
 }
 
+func TestFormatDataProviderUpdateTemplate(t *testing.T) {
+	dir := "format_data"
+	template, err := readTestData(dir, "config.json")
+	if err != nil {
+		panic(err)
+	}
+
+	source, err := readTestData(dir, "source.json")
+	if err != nil {
+		panic(err)
+	}
+
+	result, err := readTestData(dir, "result.json")
+	if err != nil {
+		panic(err)
+	}
+
+	provider, err := NewDefaultFormatJSONProvider(template)
+	if err != nil {
+		panic(err)
+	}
+
+	provider.AddFormatOption(createNilStringToBlankOption())
+
+	formatted, err := provider.FormatJSONData(source)
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println(string(formatJSON(template)))
+	fmt.Println(string(formatJSON(source)))
+	fmt.Println(string(formatJSON(formatted)))
+	assert.Equal(t, formatJSON(result), formatJSON(formatted))
+
+	templateNew, err := readTestData(dir, "config_update_template.json")
+	if err != nil {
+		panic(err)
+	}
+
+	resultNew, err := readTestData(dir, "result_update_template.json")
+	if err != nil {
+		panic(err)
+	}
+
+	if err = provider.UpdateTemplate(templateNew); err != nil {
+		panic(err)
+	}
+
+	formattedNew, err := provider.FormatJSONData(source)
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println(string(formatJSON(templateNew)))
+	fmt.Println(string(formatJSON(source)))
+	fmt.Println(string(formatJSON(formattedNew)))
+	assert.Equal(t, formatJSON(resultNew), formatJSON(formattedNew))
+}
+
 func createNilStringToBlankOption() JSONFormatOption {
 	return JSONFormatOption{
 		FunctionName:   "nil_string_to_blank",
